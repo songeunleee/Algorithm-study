223cbf8627de27c01f719dbc1c975daea262bd49
songeunleee <80832015+songeunleee@users.noreply.github.com>
2023-03-05 03:25:26 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.71 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,31 +1,23 @@
 class Solution {
     public String solution(String s) {
-       String answer = "";
-    	
-    	String[] arr = s.split(" ");
-    	for(int i=0; i<arr.length; i++) {
-    		String now = arr[i];
-    		
-    		if(arr[i].length() == 0) {
-    			answer += " ";
-    		} 
-    		
-    		else {   		
-    			answer += now.substring(0, 1).toUpperCase();
-    			
-    			answer += now.substring(1, now.length()).toLowerCase();
-    			
-    			answer += " ";
-    		}
-    		
-    	}
-    	
-    	
-    	if(s.substring(s.length()-1, s.length()).equals(" ")){
-    		return answer;
-    	}
-    	
-    	
-        return answer.substring(0, answer.length()-1);
+        String answer = "";
+        String tmp[] = s.split(" ");
+        
+        for(int i=0;i<tmp.length;i++){
+            if(tmp[i].length()==0){
+                answer+=" ";
+            }else{                
+             answer += tmp[i].substring(0,1).toUpperCase();
+             answer += tmp[i].substring(1,tmp[i].length()).toLowerCase();
+            answer += " ";
+            
+            }                                                  
+        }
+        if(s.substring(s.length()-1,s.length()).equals(" ")){
+            return answer;
+        }
+   	answer = answer.substring(0,answer.length()-1);
+        return answer;
     }
 }
+    
