41c61f52eadfb9494e6438c3cab5f38e8744e2a5
songeunleee <80832015+songeunleee@users.noreply.github.com>
2023-08-05 02:58:45 +0900

    [level 1] Title: 시저 암호, Time: 32.59 ms, Memory: 97.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        
+         for(int i =0;i<s.length();i++){
+             if(s.charAt(i)  + n > 'Z' && s.charAt(i)   < 'a' ) {
+             answer += (char)( s.charAt(i)+n -26);  
+              
+                 continue;
+                
+             } if(s.charAt(i)  + n > 'z' ) {
+             answer += (char)( s.charAt(i)+n -26);  
+                 continue;
+                
+             } if(s.charAt(i) == ' ' ){
+                 answer += ' ';
+                 continue;
+             }
+                 answer += (char)(s.charAt(i) + n);
+                   
+           
+       }
+        
+        return answer;
+    }
+}
