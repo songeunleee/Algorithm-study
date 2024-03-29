6f2adedbfba613cd6bfa2b19a22291b09c5a8838
songeunleee <80832015+songeunleee@users.noreply.github.com>
2024-03-29 17:47:55 +0900

    [level 3] Title: 네트워크, Time: 2.34 ms, Memory: 68.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Solution {
+    static boolean[] visited;
+    static ArrayList<Integer>[] A;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        A = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            A[i] = new ArrayList<>();
+        }
+        for(int i =0; i<computers.length;i++){
+            for(int j =0;j<computers[0].length;j++){
+                if(i==j) continue;
+                if(computers[i][j] == 1){
+                   A[i].add(j);
+                   A[j].add(i);
+                }
+            }
+        }
+        int count = 0;
+        visited = new boolean[n];
+        for(int i =0;i<n;i++){
+            if(!visited[i]){
+                count++;
+                DFS(i);
+            }
+        }
+        
+        
+        for(int i =0;i<n;i++){
+            System.out.println(A[i]);
+        }
+        
+        return count;
+    }
+    
+    void DFS(int v){
+        if(visited[v]){
+            System.out.println(v);
+            return;
+        }
+        
+        visited[v] = true;
+        for(int i : A[v]){
+            //System.out.print( i + " ");
+            if(!visited[i]){
+                DFS(i);
+            }
+        }
+    }
+}
