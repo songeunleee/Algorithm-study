d259bab9c9a06aa16ba40b4e9ada2c5c25410f89
songeunleee <80832015+songeunleee@users.noreply.github.com>
2024-04-12 16:58:48 +0900

    [level 2] Title: 프로세스, Time: 0.60 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,29 +1,23 @@
 import java.util.*;
 class Solution {
-     int max = 0;
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int max = 0;
         ArrayList<Integer> arr = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
         
         for(int i =0;i<priorities.length;i++){
             arr.add(priorities[i]);
-            map.put(i,priorities[i]);
             q.add(i);
         }
         
-        
-       // max(q,priorities);
         Collections.sort(arr,Collections.reverseOrder());
-      //  System.out.println(arr);
         int maxI = 0;
         while(!q.isEmpty()){
             max = arr.get(maxI);
             int now = q.poll();
             if(priorities[now] < max) q.add(now);
             else if(priorities[now] == max){
-              
               answer++;
                 maxI++;
               if(now == location) return answer;
@@ -32,16 +26,6 @@ class Solution {
         }
         
         return answer;
-    }
-    
-    void max(Queue<Integer> q, int[] priorities){
-          while(!q.isEmpty()){
-              if(max<priorities[q.peek()]) max = priorities[q.poll()];
-              else q.poll();
-          }
-        
-         
-        
     }
     
 }
